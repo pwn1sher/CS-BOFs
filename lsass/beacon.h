8266675a5473f4b7145482fb79189df4d38e2d9b
Pwnisher <ethancruize@gmail.com>
2021-01-19 20:48:26 +0530

    Update beacon.h

--- lsass/beacon.h
+++ lsass/beacon.h
@@ -8,6 +8,36 @@
  */
 
 /* data API */
+
+void mycopy(char* dst, const char* src, int size) {
+	int x;
+	for (x = 0; x < size; x++) {
+		*dst = *src;
+		dst++;
+		src++;
+	}
+}
+
+char mylc(char a) {
+	if (a >= 'A' && a <= 'Z') {
+		return a + 32;
+	}
+	else {
+		return a;
+	}
+}
+
+BOOL mycmpi(char* a, char* b) {
+	while (*a != 0 && *b != 0) {
+		if (mylc(*a) != mylc(*b))
+			return FALSE;
+		a++;
+		b++;
+	}
+
+	return TRUE;
+}
+
 typedef struct {
 	char * original; /* the original buffer [so we can free it] */
 	char * buffer;   /* current pointer into our buffer */
